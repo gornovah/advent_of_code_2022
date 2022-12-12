9dd2e3876f70d5b009b8de159b20114a7c7f3f1f
Domingo <domingo.espinosa@code.seat>
2022-12-12 21:07:14 +0100

    second part day 3

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -5,7 +5,17 @@ import load
 fun main() {
     val loadedData: List<String> = load("/day-3.txt")
     println("Solution to Day 3, Part 1 is '${day3FirstPart(loadedData)}'")
+    println("Solution to Day 3, Part 2 is '${day3SecondPart(loadedData)}'")
 
+}
+
+fun day3SecondPart(loadedData: List<String>): String {
+    return loadedData.asSequence()
+        .map { Rucksack(it) }
+        .chunked(3)
+        .map { it.first().findCommonElement(it.subList(1, it.size)) }
+        .sumOf { it.obtainPriority() }
+        .toString()
 }
 
 fun day3FirstPart(loadedData: List<String>): Int {
--- src/main/kotlin/day3/Rucksack.kt
+++ src/main/kotlin/day3/Rucksack.kt
@@ -1,10 +1,21 @@
 package day3
 
-class Rucksack(content: String) {
+class Rucksack(val content: String) {
     private val firstPartOfLine = content.toCharArray(0, (content.length / 2))
     private val secondPartOfLine = content.toCharArray((content.length / 2))
 
     fun findSimilarItem(): Char {
         return firstPartOfLine.toSet().intersect(secondPartOfLine.toSet()).first()
+    }
+
+    fun findCommonElement(rucksacks: List<Rucksack>): Char {
+        var commonElements = this.content.toSet()
+
+        rucksacks.forEach { rucksack ->
+            commonElements = commonElements.intersect(rucksack.content.toSet())
+        }
+
+        return commonElements.first()
+
     }
 }
